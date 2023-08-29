8eccb8dc11a297b25f0a24ddad8c2ad09a05c65a
ashrrithk <ashrrithkarunaakar@gmail.com>
2023-08-29 21:08:34 +0530

    project init and folder setup done

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1>Dashboard Pag (Protected)</h1>
+    </div>
+  );
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import exp from "constants";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Landing Page (UnProtected)</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
