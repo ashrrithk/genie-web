2b26fbe533b47c0329211b6a5914745992ad8bf3
ashrrithk <ashrrithkarunaakar@gmail.com>
2023-08-29 21:46:02 +0530

    auth added

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function DashboardPage() {
   return (
     <div>
       <h1>Dashboard Pag (Protected)</h1>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,19 @@
+import { Button } from "@/components/ui/button";
 import exp from "constants";
+import Link from "next/link";
 
 const LandingPage = () => {
   return (
     <div>
       <h1>Landing Page (UnProtected)</h1>
+      <div>
+        <Link href="/sign-in">
+          <Button>Login</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
     </div>
   );
 };
